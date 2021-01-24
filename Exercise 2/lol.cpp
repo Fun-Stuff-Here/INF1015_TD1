0221de88a3617cc302abf3642f112cb4facc4ff0
Eli-michaud <47183913+Eli-michaud@users.noreply.github.com>
2021-01-23 21:57:59 -0500

    exercice 2

--- Exercise 2/lol.cpp
+++ Exercise 2/lol.cpp
@@ -1,20 +1,37 @@
-// Exercise 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+double calculateLimit(int precision)
+{
+	double u0 = 1;
+	double term = 1;
+	double nextTerm = sqrt(term + 2);
+	double precisionFactor = pow(0.1, precision);
+
+	while (abs(term - nextTerm) > precisionFactor)
+	{
+		term = nextTerm;
+		nextTerm = sqrt(term + 2);
+	}
+	return nextTerm;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int precision = 3;
+	double limit = calculateLimit(precision);
+
+
+	double truncatedLimit = trunc(limit * pow(10, precision)) / pow(10, precision);
+
+	cout << "La limite arondie de la suite à " << precision << " décimales près est ";
+	cout << fixed << setprecision(precision);
+	cout << limit << endl;
+
+	cout << "La limite tronquée de la suite à " << precision << " décimales près est ";
+	cout << truncatedLimit;
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
